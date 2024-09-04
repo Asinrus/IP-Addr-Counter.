127fd392439a8ee840a98c7be44b9ba805cbf918
Arkadii_Osheev <arkadii.osheev@tvh.com>
2024-09-04 15:18:07 +0400

    save filechannell solution

--- src/main/java/org/github/ipfilereader/SingleThreadUniqueCounter.java
+++ src/main/java/org/github/ipfilereader/SingleThreadUniqueCounter.java
@@ -1,12 +1,21 @@
 package org.github.ipfilereader;
 
 import org.github.ipfilereader.exception.InterruptedReadOperation;
+import org.github.ipfilereader.set.AllIpAddressesSet;
 import org.github.ipfilereader.set.BloomFilter;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.channels.FileChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.util.BitSet;
 import java.util.List;
 import java.util.stream.IntStream;
@@ -17,28 +26,18 @@ public class SingleThreadUniqueCounter implements UniqueCounter {
 
     @Override
     public long numOfUniqueLinesIn(File file) {
-        int size = 4;
-        List<BloomFilter> filters = IntStream.range(0, size)
-                .mapToObj(i -> new BloomFilter(new BitSet(1 << 30),HASH_FUNCTIONS))
-                .toList();
-        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                insert(line, filters);
+        AllIpAddressesSet allIpAddressesSet = new AllIpAddressesSet();
+
+        try (FileChannel fc = (FileChannel) Files.newByteChannel(file.toPath(), StandardOpenOption.READ)) {
+            var chunkIterator = new ChunkIterator(fc);
+            var ipProccessor = new IpProcessor(allIpAddressesSet::add);
+            while (chunkIterator.hasNext()) {
+                ipProccessor.process(chunkIterator.next());
             }
         } catch (IOException e) {
-            throw new InterruptedReadOperation(e);
+            throw new RuntimeException(e);
         }
-        return filters.parallelStream()
-                .map(filter -> (long) filter.size())
-                .reduce(Long::sum)
-                .orElse(0L);
-    }
-
-    private void insert(String line, List<BloomFilter> filters) {
-        IpHash ipHash = IpHash.form(line);
-        int idx = Math.abs(line.hashCode()) % filters.size();
-        filters.get(idx).put(ipHash);
+        return allIpAddressesSet.size();
     }
 }
 
